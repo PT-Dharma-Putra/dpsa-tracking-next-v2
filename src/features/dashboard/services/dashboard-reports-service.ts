602d19f488810d9d3b1d394382465b05d1394cb2
Muhammad Zakiyuddin <zakinonstop1@gmail.com>
2026-08-05 10:46:45 +0700

    update filter divisi on dashboard

--- src/features/dashboard/services/dashboard-reports-service.ts
+++ src/features/dashboard/services/dashboard-reports-service.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "@/lib/axios";
 
-export const getReportsData = async () => {
-  const response = await axiosInstance.get('/dashboard/reports');
+export const getReportsData = async (params?: { client_id?: string; month?: string; year?: string }) => {
+  const response = await axiosInstance.get('/dashboard/reports', { params });
   return response.data;
 };
